f1022152c501ac745da2f761c8cc71859ba2bcc0
Melakuera <musabaeveldiar@gmail.com>
2022-05-06 22:32:24 +0600

    init

---
+++ src/main/java/io/melakuera/ourtube/OurtubeApplication.java
@@ -0,0 +1,13 @@
+package io.melakuera.ourtube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurtubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurtubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/melakuera/ourtube/OurtubeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.melakuera.ourtube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurtubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
